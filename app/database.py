781b238c148bbd6aedf7cdea30cbaa6c82e83209
dAnkc0der <69014454+dAnkc0der@users.noreply.github.com>
2023-10-25 00:23:18 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,15 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 
-# DATABASE_HOSTNAME = os.environ["DATABASE_HOSTNAME"]
-# DATABASE_PORT = os.environ["DATABASE_PORT"]
-# DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
-# DATABASE_NAME = os.environ["DATABASE_NAME"]
-# DATABASE_USERNAME = os.environ["DATABASE_USERNAME"]
-
 DATABASE_URL = os.environ.get("DATABASE_URL")
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}'
-# postgresql://databasepostgres_hagz_user:l3q6yH5wuoaSLjx4jrRXCeOBaAREOULT@dpg-ckrrumo1hnes738ub1ug-a/databasepostgres_hagz
 
 engine = create_engine(DATABASE_URL)
 
