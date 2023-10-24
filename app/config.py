16364a83f2642d8909dbd43dbc07d1cbc455741f
dAnkc0der <utkarshverma007@outlook.com>
2023-10-24 23:58:54 +0530

    None

--- app/config.py
+++ app/config.py
@@ -2,11 +2,12 @@ from pydantic_settings import BaseSettings
 from app import database
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
+    # database_hostname: str
+    # database_port: str
+    # database_password: str
+    # database_name: str
+    # database_username: str
+    DATABASE_URL: str
 
     class Config:
         env_file = ".env"
--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
-import os
+
 
 # DATABASE_HOSTNAME = os.environ["DATABASE_HOSTNAME"]
 # DATABASE_PORT = os.environ["DATABASE_PORT"]
@@ -10,11 +11,11 @@ import os
 # DATABASE_NAME = os.environ["DATABASE_NAME"]
 # DATABASE_USERNAME = os.environ["DATABASE_USERNAME"]
 
-SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ.get("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URL = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}'
 # postgresql://databasepostgres_hagz_user:l3q6yH5wuoaSLjx4jrRXCeOBaAREOULT@dpg-ckrrumo1hnes738ub1ug-a/databasepostgres_hagz
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
